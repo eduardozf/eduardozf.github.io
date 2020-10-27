16c9b247ece776e0a6bf5c7330021f7edd0c9455
Eduardo ZF <eduardozf1@gmail.com>
2020-10-27 16:37:08 -0300

    🚀 Twitter Web
    - First commit

---
+++ precache-manifest.c68faca6613fe3f38bc9d24142e2c289.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3ca106fb315456a5c78fc83c810509f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4da31986315bc753e3bd",
+    "url": "/static/js/2.1476bc91.chunk.js"
+  },
+  {
+    "revision": "3c73c585782ac05880c0f89bcfdbba5a",
+    "url": "/static/js/2.1476bc91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b04d4b8f324a01a4941b",
+    "url": "/static/js/main.f8ce78ee.chunk.js"
+  },
+  {
+    "revision": "6981919806c089cef906",
+    "url": "/static/js/runtime-main.bfca2edd.js"
+  }
+]);
